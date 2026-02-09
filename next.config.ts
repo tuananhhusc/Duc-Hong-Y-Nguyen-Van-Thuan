08ab836ffb1c2d67f08bdbdaa0bb95b230a25cec
quynhanh777 <tuananhqhvl777@gmail.com>
2026-02-09 15:40:44 +0700

    feat: Add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'Duc-Hong-Y-Nguyen-Van-Thuan';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Base path for GitHub Pages
+  // If you are deploying to a custom domain (e.g. example.com), remove this line
+  // If you are deploying to username.github.io/repo-name, keep this line
+  basePath: isProd ? `/${repoName}` : '',
 };
 
 export default nextConfig;
